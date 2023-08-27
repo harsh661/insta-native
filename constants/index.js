d67cc21a4e19daa5f6c84d0e153150fe2b273ccb
harsh661 <plasmaharsh@gmail.com>
2023-08-27 09:41:40 +0530

    misc: add some temporary data

---
+++ constants/index.js
@@ -0,0 +1,22 @@
+export const users = [
+    {
+        name: 'User 1',
+        image: 'https://i.ibb.co/bWcmyRb/user1.png'
+    },
+    {
+        name: 'User 2',
+        image: 'https://i.ibb.co/C2LZS7J/user2.png'
+    },
+    {
+        name: 'User 3',
+        image: 'https://i.ibb.co/9q0GPXX/user3.png'
+    },
+    {
+        name: 'User 4',
+        image: 'https://i.ibb.co/YtdBYbx/user4.png'
+    },
+    {
+        name: 'User 5',
+        image: 'https://i.ibb.co/1Ktfx5v/user5.png'
+    },
+]
